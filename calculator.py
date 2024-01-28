47385cc8a08e4d3c5d6d97b9d7400ad574cd5f98
Thier02 <tbelanger099@gmail.com>
2024-01-27 19:27:45 -0500

    Adding multiplication feature

--- calculator.py
+++ calculator.py
@@ -6,11 +6,13 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
-
+def multiply(a, b):
+    return a * b
 
 def main():
     print(add(10, 2))
     print(sub(10, 2))
+    print(multiply(10, 2))
 
 if __name__ == '__main__':
     main()
