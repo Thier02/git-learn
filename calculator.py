103e02484a9cfca1436009d9c769d1529a2928f3
Thier02 <tbelanger099@gmail.com>
2024-01-27 19:22:55 -0500

    Creating calculator.py and adding add and sub functions

---
+++ calculator.py
@@ -0,0 +1,17 @@
+
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+
+
+def main():
+    print(add(10, 2))
+    print(sub(10, 2))
+
+if __name__ == '__main__':
+    main()
+
